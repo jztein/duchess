7aff9c3afbf94c15e5cca6b9339e2082635dd6aa
jztein <jiayuaw@gmail.com>
2014-07-01 19:53:59 -0700

    modular and change price

---
+++ Stalker.py
@@ -0,0 +1,128 @@
+import httplib
+import urllib2, urllib, cookielib, urlparse
+import base64, cStringIO, json
+
+from google.appengine.api import urlfetch
+
+f = open('admin.secret', 'r')
+secrets = f.readlines()
+f.close()
+
+API_KEY = secrets[0]
+SHARED_SECRET = secrets[1]
+SHOP_URL = 'https://hedgehog.myshopify.com/admin'
+GEN_SHOP_URL = 'hedgehog.myshopify.com'
+
+auth = base64.b64encode("%s:%s" % (API_KEY, SHARED_SECRET))
+HEADERS = { 'Authorization': "Basic " + auth }
+JSON_HEADERS = { 'Authorization': "Basic " + auth,
+                 'Content-Type': 'application/json'}
+
+BASELINE_PRICE = 5.00
+
+class UrlFetchException(IOError):
+    pass
+
+class TestException(IOError):
+    pass
+
+class ShopifyException(ValueError):
+    pass
+
+def putProductData(jsonValue, pid):
+
+    goat = json.dumps(jsonValue)
+
+    b = urlfetch.fetch(url="%s/products/%s.json" % (SHOP_URL, pid),
+                       method=urlfetch.PUT,
+                       payload=goat,
+                       headers=JSON_HEADERS)
+    print "##### put product data"
+    print b.status_code
+    print "##### end: put product data"
+
+
+def deleteProduct(pid):
+    b = urlfetch.fetch(url="%s/products/%s.json" % (SHOP_URL, pid),
+                       method=urlfetch.DELETE,
+                       headers=HEADERS)
+    print "#^#^# deleting product"
+    print b.status_code
+    print "#^#^# END deleting product"
+
+
+def testAddProduct():
+    goat = json.dumps({"product": {"title": "Burton Custom Freestlye 151","body_html": "<strong>Good snowboard!</strong>","vendor": "Burton","product_type": "Snowboard","tags": "Barnes & Noble, John's Fav, \"Big Air\""}})
+
+    b = urlfetch.fetch(url="%s/products.json" % SHOP_URL,
+                       method=urlfetch.POST,
+                       payload =goat,
+                       headers=JSON_HEADERS)
+    print "##### test add product"
+    print b.status_code
+    print b.content
+    print "##### end test add product"
+
+    products = getAllProducts()
+    for product in products:
+        if product['title'] == 'Burton Custom Freestlye 151':
+            return
+
+    raise TestException('did not add product correctly')
+        
+
+def getAllProducts():
+    url = '%s/products.json' % SHOP_URL
+    resp = urlfetch.fetch(url, headers=HEADERS)
+    if resp.status_code == 200:
+        jsonFile = cStringIO.StringIO(resp.content)
+        # decoded json into dictionary-and-list format
+        decoded = json.load(jsonFile)
+        print "##### get all products"
+        print "Num products:", len(decoded['products'])
+        print "##### END get all products"
+        return decoded['products']
+    else:
+        raise UrlFetchException('could not fetch in get all products')
+
+# change price of product named $title to $newPrice
+def changePrice(products, title, newPrice=-1, discount=0.10):
+    gotProduct = False
+    for product in products:
+        if product['title'] == title:
+            if newPrice == -1:
+                newPrice = (1.0 - discount) * float(product['variants'][0]['price'])
+            product['variants'][0]['price'] = newPrice
+            newProduct = product
+            gotProduct = True
+            break
+
+    if gotProduct:
+        print ">> putting new price"
+        pid = newProduct['id']
+
+        if float(newPrice) < BASELINE_PRICE:
+            deleteProduct(pid)
+            print "!!! Deleted product:", newProduct['title']
+            return
+
+        newJson = {}
+        newJson['product'] = newProduct
+        putProductData(newJson, pid)
+    else:
+        raise ShopifyException('could not change price of product %s' % title)
+    
+    # comment out if TEST-ing
+    return
+
+    # TEST: check stuff
+    products = getAllProducts()
+    for product in products:
+        if product['title'] == title:
+            if str(product['variants'][0]['price']) != "%.2f" % newPrice:
+                print "ERROR"
+                print "Wrong price:", product['variants'][0]['price']
+                print "Expected price: %.2f" % newPrice
+                raise TestException('price not altered correctly')
+            else:
+                print "New price:", product['variants'][0]['price']
--- duchess.py
+++ duchess.py
@@ -4,10 +4,11 @@ import urllib, urllib2, cookielib, urlparse
 import base64
 import cStringIO
 import json
+import time
 
 from google.appengine.api import urlfetch
 
-import shTalker
+import Stalker
 
 SHOP_URL = 'hedgehog.myshopify.com'
 f = open('index.html')
@@ -16,17 +17,44 @@ f.close()
 
 class MainPage(webapp2.RequestHandler):
     def head(self):
+        print "\n\n IN THE HEAD of Main Page\n\n"
+        self.get(self)
+
+    def get(self):
+        self.response.out.write(html)
+
+
+class DutchPage(webapp2.RequestHandler):
+    def head(self):
         print "\n\n IN THE HEAD \n\n"
         self.get(self)
 
     def get(self):
-        shTalker.getAllProducts()
-        self.response.out.write(html)
+        try:
+            products = Stalker.getAllProducts()
+            try:
+                i = 0
+                for product in products:
+                    print ">>>", product['title']
+                    Stalker.changePrice(products, product['title'],
+                                        discount=0.10)
+                    # shopify's api limit is avg 2 per second
+                    if i % 2 == 1:
+                        print "SLEEEEEEEPPiiingg ..... "
+                        time.sleep(1)
+                    i += 1
+            except BaseException as e:
+                print "ERROR in DUTCH PAGE: Couldn't change price"
+                print e.read()
+        except BaseException as e:
+            print "ERROR in DUTCH PAGE: couldn't get all products"
+            print e.read()
 
-
+        print "Did a dutch"
 
 
 application = webapp2.WSGIApplication([
         ('/', MainPage),
+        ('/dutch', DutchPage),
 ], debug=True)
 
